74e945865554cafd3696bbdf46430105024125d4
root <root@raspberrypi.(none)>
2018-07-29 04:31:46 +0200

    Restore, rebuild, rename

---
+++ dsmrp1spot-manual.php
@@ -0,0 +1,71 @@
+<?PHP
+
+// Include config file
+// *** P1 path file     * $p1path $p1latestfile
+// *** DATABASE         * $dbserver $dbusername $dbpassword $dbname
+// *** PvOutput.org     * $pvoapi $pvosid
+include '/usr/local/bin/dsmrp1spot/p1spot.conf';
+$curlapi = "X-Pvoutput-Apikey: ".$pvoapi;
+$curlsid = "X-Pvoutput-SystemId: ".$pvosid;
+
+$updatestart = "1532815249"; $updateend ="1532815249";
+
+// #################################################################
+//
+$con=mysqli_connect($dbserver,$dbusername,$dbpassword,$dbname);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+//
+$sql="SELECT ";
+$sql.="TimeStamp, EnergyimportDaily";
+$sql.=" FROM P1device ";
+// $sql.=" WHERE TimeStamp > 1532815249";
+$sql.=" WHERE TimeStamp BETWEEN ".$updatestart." AND ".$updateend;
+$sql.=" ORDER BY TimeStamp ASC ";
+//$sql.=" LIMIT 10 ";
+
+if ($result=mysqli_query($con,$sql))
+  {
+  // Fetch one and one row
+  while ($row=mysqli_fetch_row($result))
+    {
+    //printf ("%s (%s)\n",$row[0],$row[1]);
+    //echo $row[0]."|".date('Ymd', $row[0])."|".date('H:i', $row[0])."|".$row[1]."\n";
+	
+	$curldate = date('Ymd', $row[0]);
+	$curltime = date('H:i', $row[0]);
+    $curlv3 = $row[1] ;
+    
+    echo "curl -s -d d=".$curldate." -d t=".$curltime." -d v3=".$curlv3;
+	exec ('curl -s -d "d='.$curldate.'" -d "t='.$curltime.'" -d "v3='.$curlv3.'" -H "'.$curlapi.'" -H "'.$curlsid.'" '.$pvourladd.'', $cout, $cret);
+    echo " ".$cout[0]." ".$cret."\n";
+    }
+  // Free result set
+  mysqli_free_result($result);
+}
+
+mysqli_close($con);
+
+
+// #################################################################
+// *** Post data directly to PVoutput.org via CURL
+/*
+$curldate = date('Ymd', $p1timestampunix);
+$curltime = date('H:i', $p1timestampunix);
+$curlapi = "X-Pvoutput-Apikey: ".$pvoapi;
+$curlsid = "X-Pvoutput-SystemId: ".$pvosid;
+$curlv3 = $EnergyimportDaily;
+
+echo "|".date('Ymd', $lastp1timestampunix)."|".date('H:i', $p1timestampunix)."|".$pvoapi."|".$pvosid."|".$pvourladd."|";
+echo "\n";
+echo "|".$curldate."|".$curltime."|".$curlv3."|".$curlapi."|".$curlsid."|".$EnergyimportDaily;
+echo "\n";
+//
+exec ('curl -s -d "d='.$curldate.'" -d "t='.$curltime.'" -d "v3='.$curlv3.'" -H "'.$curlapi.'" -H "'.$curlsid.'" '.$pvourladd.'');
+*/
+?> 
+
+
---
+++ dsmrp1spot-read.py
@@ -0,0 +1,50 @@
+### DSMR P1 read from port via serial
+versie = "0.2"
+import sys
+import serial
+
+### Set COM port config
+ser = serial.Serial()
+
+ser.baudrate = 115200
+ser.bytesize=serial.SEVENBITS
+ser.parity=serial.PARITY_NONE
+ser.stopbits=serial.STOPBITS_ONE
+ser.xonxoff=0
+ser.rtscts=0
+ser.timeout=20
+
+ser.port="/dev/ttyUSB0"
+
+### p1_count counter for p1.datagram length in case of error read
+p1_count=0
+p1_line=''
+
+##############################################################################
+# Main program
+##############################################################################
+
+### Open COM port
+try:
+    ser.open()
+except:
+    sys.exit ("Error opening %s"  % ser.name)
+
+### Read 1 line van de seriele poort
+while p1_line[:1] <> "!":
+    try:
+        p1_raw = ser.readline()
+    except:
+        sys.exit ("Serial port %s cannot be read." % ser.name )
+
+### put raw data in string and print
+    p1_str=str(p1_raw)
+    p1_line=p1_str.strip()
+    print (p1_line)
+    p1_count = p1_count +1
+
+### Close port and show status
+try:
+    ser.close()
+except:
+    sys.exit ("Oops %s. program aborted. Could not clode serial port." % ser.name )
